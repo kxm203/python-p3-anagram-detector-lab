fa9b4af9a456e87fb7b35150d8cc7cd16b7d46d0
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-02-27 13:14:29 -0500

    Pretty cool!  Some comparisons which is nice.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+
+    def __init__ (self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        anagrams = []
+        for words in word_list:
+            if sorted(words.lower()) == sorted(self.word.lower()) and words.lower() != self.word.lower():
+                anagrams.append(words)
+        return anagrams
